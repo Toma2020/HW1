08c73e26bb8c20929334a2d85ced9e4121a445c0
Tom <Tom@Mail.com>
2020-05-17 16:14:54 +0300

    important

---
+++ library/src/main/kotlin/HttpHandler.kt
@@ -0,0 +1,39 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+
+
+class HttpHandler
+{
+
+    fun sendGetRequest(requestURL: String): StringBuffer
+    {
+        //val mURL = URL("https://postman-echo.com/get?foo1=bar1&foo2=bar2")
+        val mURL = URL(requestURL)
+
+        with(mURL.openConnection() as HttpURLConnection)
+        {
+            // optional default is GET
+            requestMethod = "GET"
+
+            println("URL : $url")
+            println("Response Code : $responseCode")
+
+            BufferedReader(InputStreamReader(inputStream)).use {
+                val response = StringBuffer()
+
+                var inputLine = it.readLine()
+                while (inputLine != null) {
+                    response.append(inputLine)
+                    inputLine = it.readLine()
+                }
+                it.close()
+                println("Response : $response")
+                return response
+            }
+        }
+    }
+
+
+}
